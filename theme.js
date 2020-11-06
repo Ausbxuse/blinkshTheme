3b8d84ebd9c56a72639e77590cd36460c37d8a29
peter <fantasticpeterzhao@gmail.com>
2020-11-05 16:28:03 -0800

    first build

---
+++ theme.js
@@ -0,0 +1,21 @@
+t.prefs_.set("color-palette-overrides", [
+  "#232323",
+  "#ff000f",
+  "#8ce10b",
+  "#ffb900",
+  "#7aa6da",
+  "#bd93f9",
+  "#00d8eb",
+  "#ffffff",
+  "#444444",
+  "#ff2740",
+  "#abe15b",
+  "#ffd242",
+  "#61afef",
+  "#bd93f9",
+  "#67fff0",
+  "#ffffff",
+]);
+t.prefs_.set("foreground-color", "#fffaf4");
+t.prefs_.set("background-color", "#0e1019");
+t.prefs_.set("cursor-color", "rgba(255,250,244,1)");
